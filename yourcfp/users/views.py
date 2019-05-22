ec1e69d704241fb390c7c2d1d6941ed4379f9cd4
sujay <sujay@localhost.localdomain>
2019-05-22 21:05:19 +0530

    implemented user registeration

--- yourcfp/users/views.py
+++ yourcfp/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
 
-# Create your views here.
+# Create your views here
+
+class RegisterView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'users/user_register_form.html'
+    success_url = reverse_lazy('home:index')
