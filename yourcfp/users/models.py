09efacca2d328e477db64aaa1f84771a36b131b3
sujay <sujay@localhost.localdomain>
2019-05-22 23:17:01 +0530

    added a user profile model

--- yourcfp/users/models.py
+++ yourcfp/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    blog_url = models.URLField(blank=True)
+    twitter_handle = models.CharField(max_length=15, blank=True)
+    location = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.username
