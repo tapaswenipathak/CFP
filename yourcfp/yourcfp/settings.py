a2b9300691a2c1686b2a253392d49e8ab2eabee6
sujay <sujay@localhost.localdomain>
2019-05-25 23:41:12 +0530

    created a profiles app

--- yourcfp/yourcfp/settings.py
+++ yourcfp/yourcfp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles.apps.ProfilesConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
